4167fe8b0fc9db0dd81226736140a27f065dbb8d
EdvardGrodem <edvard.grodem@gmail.com>
2019-01-09 16:48:44 +0100

    Add comments

--- Testrig_Arduino_Code/Propeller Test Stand/src/CurrentSensor.h
+++ Testrig_Arduino_Code/Propeller Test Stand/src/CurrentSensor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <Arduino.h>
 
+//Made to work with the ACS758 current sensor
+//The sensor mesures current and outputs voltage
 class CurrentSensor {
 private:
   float current;
@@ -8,8 +10,16 @@ private:
   float gain;
   int analog_pin;
 public:
+
+  //Initialize currentsensor on set analog pin
   CurrentSensor(int analog_pin_p);
+
+  //Reads the analog inputs, updates the current value and returns the raw data
   int sample();
+
+  //Returns the value of the current
   float getCurrent();
+
+  //Takes two datapoints and sets the gain of the currentsensor
   void calibrate(int raw_1, float real_1, int raw_2, float real_2);
 };
